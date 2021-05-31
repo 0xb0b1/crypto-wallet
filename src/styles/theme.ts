14eb80422629bf260d2a42da193c875249c581dc
0xb0b1 <vcente82@gmail.com>
2021-05-31 14:54:49 -0300

    ...

---
+++ src/styles/theme.ts
@@ -0,0 +1,17 @@
+import { extendTheme } from "@chakra-ui/react";
+
+export const theme = extendTheme({
+  fonts: {
+    heading: "Poppins",
+    body: "Poppins",
+  },
+
+  styles: {
+    global: {
+      body: {
+        bg: "gray.50",
+        color: "gray.900",
+      },
+    },
+  },
+});
