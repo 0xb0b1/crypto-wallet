fece541b3039f36a644724ff886e49544d9253d0
0xb0b1 <vcente82@gmail.com>
2021-06-01 08:23:45 -0300

    ...

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,8 @@ import {
   Divider,
 } from "@chakra-ui/react";
 
+import Link from "next/link";
+
 export const Home = () => {
   return (
     <Box w="100vw" h="100vh" bgGradient="linear(to-t, gray.300, blue.200)">
@@ -30,9 +32,11 @@ export const Home = () => {
         </Center>
         <Image src="/home-bitcoin.png" alt="Home background" w="40rem" />
 
-        <Button type="button" variant="filled" color="gray.900" bg="blue.300">
-          Get Started
-        </Button>
+        <Link href="/auth/signin">
+          <Button type="button" variant="filled" color="gray.900" bg="blue.300">
+            Get Started
+          </Button>
+        </Link>
       </Flex>
     </Box>
   );
